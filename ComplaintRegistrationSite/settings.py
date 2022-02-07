b3c78e2ab1328b3eb038dd6846fcd9d89e4ef60a
rohit agrawal <rohitagrawal7201@gmail.com>
2022-02-07 08:27:23 +0530

    Commit

--- ComplaintRegistrationSite/settings.py
+++ ComplaintRegistrationSite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&yqpe+@=*1no&axbh8uw(l5)k6yjlz!)s*p&-8d&b_%p-b8$hi'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'ComplaintRegistrationSite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "ComplaintRegistrationSite_db",#config('DB_NAME'),
-        'USER': "postgres",#config('DB_USER'),
-        'PASSWORD': "123zxcvbnm",#config('DB_USER_PASSWORD'),
-        'HOST': "localhost"#config('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
     }
 }
 
@@ -141,9 +141,9 @@ MESSAGE_TAGS = {
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # email stuff
-EMAIL_HOST = "smtp.gmail.com"#config("EMAIL_HOST"),#os.environ.get('EMAIL_HOST')
-EMAIL_HOST_USER = "rohit786agrawal786@gmail.com"#config("EMAIL_HOST_USER"),#os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = "rohit786agrawal786@gmail.com"#config("DEFAULT_FROM_EMAIL"),#os.environ.get('DEFAULT_FROM_EMAIL')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = "rohit786786"#config("EMAIL_HOST_PASSWORD"),#os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
